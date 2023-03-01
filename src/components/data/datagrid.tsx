e33345f970d04b17d66f34db11a05b3599961813
aniceto-jolela <anicetojosejolelaj@gmail.com>
2023-03-01 16:31:16 +0100

    Add method  update.

--- src/components/data/datagrid.tsx
+++ src/components/data/datagrid.tsx
@@ -4,27 +4,27 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Grid from "@mui/material/Grid";
 import api from "../../api";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 100 },
-  { field: "Cod", headerName: "Cod", width: 60 },
   {
     field: "Nome",
     headerName: "Nome",
     width: 300,
-    editable: true,
+    editable: false,
   },
   {
     field: "Sobrenome",
     headerName: "Sobrenome",
     width: 200,
-    editable: true,
+    editable: false,
   },
   {
     field: "Email",
     headerName: "Email",
     width: 300,
-    editable: true,
+    editable: false,
   },
 ];
 
@@ -39,6 +39,10 @@ export default function DataGridDemo() {
   const [mapUser, setMapUser] = React.useState<propsUsuario[]>([]);
   const [idUser, setIdUser] = React.useState("");
   const [updateHook, setUpdateHook] = React.useState("");
+  const [sms, setSms] = React.useState("");
+  const [nome, setNome] = React.useState("");
+  const [sobrenome, setSobrenome] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
   React.useEffect(() => {
     api
@@ -53,7 +57,6 @@ export default function DataGridDemo() {
   }, [updateHook]);
 
   function selectedRow(e?: any) {
-    console.log(" ID =" + e);
     setIdUser(e);
   }
 
@@ -68,11 +71,14 @@ export default function DataGridDemo() {
           .catch((err) => {
             console.log(err);
           })
-      : console.log(idUser);
+      : console.log("");
   }
 
-  function updateSelectRow(e:any){
-    console.log("Dados = ", e)
+  function updateSelectRow(e: any) {
+    const { id, Nome, Sobrenome, Email } = e.row;
+    setNome(Nome);
+    setSobrenome(Sobrenome);
+    setEmail(Email);
   }
 
   const rows = mapUser?.map((item, count) => ({
@@ -81,6 +87,26 @@ export default function DataGridDemo() {
     Nome: item.nome,
     Email: item.email,
   }));
+
+  const InputTextValues = {
+    nome: nome,
+    sobrenome: sobrenome,
+    email: email,
+  };
+
+  const handleSubmit = () => {
+    nome === ""
+      ? setSms("Campo obrigatório")
+      : sobrenome === ""
+      ? setSms("Campo obrigatório")
+      : api
+          .post("content/" + idUser + "", InputTextValues)
+          .then((req) => {
+            setUpdateHook(req.data._id);
+            console.log(req.data);
+          })
+          .catch((err) => console.log(err));
+  };
 
   return (
     <>
@@ -95,10 +121,60 @@ export default function DataGridDemo() {
         </Button>
       </Grid>
 
+      <Grid
+        container
+        spacing={0}
+        justifyContent="center"
+        direction="row"
+        alignItems="center"
+        style={{ background: "#FFF", paddingTop: 10, paddingBottom: 40 }}
+      >
+        <Grid item xs={6} md={3}>
+          <TextField
+            id="outlined-required"
+            label="nome"
+            defaultValue=""
+            size="small"
+            helperText={nome === "" ? sms : ""}
+            name="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <TextField
+            id="outlined-required"
+            label="Sobrenome"
+            defaultValue=""
+            helperText={sobrenome === "" ? sms : ""}
+            size="small"
+            name="sobrenome"
+            value={sobrenome}
+            onChange={(e) => setSobrenome(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <TextField
+            id="outlined-helperText"
+            label="nome@provedor.com"
+            defaultValue=""
+            size="small"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Button variant="outlined" size="small" onClick={handleSubmit}>
+            Editar
+          </Button>
+        </Grid>
+      </Grid>
+
       <Box sx={{ height: 400, width: "100%" }}>
         <DataGrid
           onSelectionModelChange={(e) => selectedRow(e)}
-          onEditRowsModelChange={(up)=>updateSelectRow(up)}
+          onRowClick={(up) => updateSelectRow(up)}
           rows={rows}
           columns={columns}
           pageSize={5}
